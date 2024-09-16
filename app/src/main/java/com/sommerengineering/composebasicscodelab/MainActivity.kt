36d5b9025f09ff20b063d6964eb347ec54398756
Drew Sommer <drew@sommerengineering.com>
2024-09-16 22:59:23 +0300

    refactor: Isolate composeable

--- app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
@@ -7,35 +7,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.sommerengineering.composebasicscodelab.ui.theme.ComposeBasicsCodelabTheme
+import androidx.compose.ui.unit.dp
+import com.sommerengineering.composebasicscodelab.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeBasicsCodelabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            BasicComposeTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeBasicsCodelabTheme {
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
         Greeting("Android")
     }
 }
@@ -48,7 +45,15 @@ fun Greeting(
     Surface(color = MaterialTheme.colorScheme.primary) {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BasicComposeTheme {
+        MyApp()
     }
 }

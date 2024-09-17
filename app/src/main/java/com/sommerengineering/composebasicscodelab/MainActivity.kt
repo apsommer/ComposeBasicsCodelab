4940b581250406eb23a12f65f56d3ef8f6494695
Drew Sommer <drew@sommerengineering.com>
2024-09-17 16:42:33 +0300

    feat: Implement large, lazy list

--- app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -80,10 +82,11 @@ fun OnboardingScreen(
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }

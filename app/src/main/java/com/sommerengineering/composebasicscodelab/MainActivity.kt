8970989cbc1147d2486b3b50069f5d731e60c6f1
Drew Sommer <drew@sommerengineering.com>
2024-09-17 16:46:21 +0300

    feat: Persist ui on device rotation

--- app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,7 +48,7 @@ class MainActivity : ComponentActivity() {
 fun MyApp(modifier: Modifier = Modifier) {
 
     // hoist state
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if (shouldShowOnboarding) {

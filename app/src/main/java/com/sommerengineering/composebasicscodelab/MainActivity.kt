fb4a6c4fe36536725beea04540639d03b1e5fe41
Drew Sommer <drew@sommerengineering.com>
2024-09-17 18:53:57 +0300

    style: Colors and fonts using material design spec

--- app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/composebasicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sommerengineering.composebasicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,9 +11,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -24,17 +23,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.sommerengineering.composebasicscodelab.ui.theme.BasicComposeTheme
-import com.sommerengineering.composebasicscodelab.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -90,7 +87,6 @@ fun Greetings(
     names: List<String> = List(1000) { "$it" }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-
         items(items = names) { name ->
             Greeting(name = name)
         }
@@ -116,7 +112,7 @@ fun Greeting(
     )
 
     Surface(
-        color = Purple80,
+        color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = modifier.padding(24.dp)) {
@@ -125,8 +121,13 @@ fun Greeting(
                     .weight(1f)
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
-                Text(color = Color.White, text = "Hello")
-                Text(color = Color.White, text = "$name!")
+                Text(text = "Hello")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { isExpanded = !isExpanded}
@@ -158,6 +159,12 @@ fun OnboardingPreview() {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
--- app/src/main/java/com/sommerengineering/composebasicscodelab/ui/theme/Color.kt
+++ app/src/main/java/com/sommerengineering/composebasicscodelab/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.sommerengineering.composebasicscodelab.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFF4B279D)
-val PurpleGrey80 = Color(0xFF7651B0)
-val Pink80 = Color(0xFF2C000E)
-
-val Purple40 = Color(0xFF2E04A6)
-val PurpleGrey40 = Color(0xFF4C3C6C)
-val Pink40 = Color(0xFF7D5260)
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
